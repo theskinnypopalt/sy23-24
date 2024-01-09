39c12b2b1ca7ce887ac197681ce748323b6e97f4
theskinnypopalt <bmste396@learner.dvusd.org>
2024-01-09 08:16:27 -0700

    code and menu strip added
    code for saving files to the computer

--- WinFormsApp1/WinFormsApp1/Form1.vb
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -2,4 +2,18 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim outFile As New IO.StreamWriter("out.txt")
+        outFile.Write(TextBox1.Text)
+        outFile.Close()
+    End Sub
 End Class
