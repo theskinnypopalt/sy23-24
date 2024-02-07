36a8fa9751b62a0614de1015eab790d30547bff8
theskinnypopalt <bmste396@learner.dvusd.org>
2024-02-07 09:22:07 -0700

    vending machiene

---
+++ vending machiene/vending machiene/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Dim cs As New coinslot
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        cs.insertQuarter()
+        TextBox1.Text = cs.total
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        cs.insertDimes()
+        TextBox1.Text = cs.total
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cs.insertDollars()
+        TextBox1.Text = cs.total
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        cs.insertPennies()
+        TextBox1.Text = cs.total
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        cs.insertNickles()
+        TextBox1.Text = cs.total
+    End Sub
+
+
+End Class
---
+++ vending machiene/vending machiene/coinslot.vb
@@ -0,0 +1,29 @@
+﻿Public Class coinslot
+    Public Property Quarters As Integer
+    Public Property Nickles As Integer
+    Public Property Dimes As Integer
+    Public Property Pennies As Integer
+    Public Property Dollars As Integer
+    Public ReadOnly Property total As Decimal
+        Get
+            Return Dollars * 1 + Quarters * 0.25 + Dimes * 0.1 + Nickles * 0.05 + Pennies * 0.01
+        End Get
+
+    End Property
+
+    Public Sub insertQuarter()
+        Quarters = Quarters + 1
+    End Sub
+    Public Sub insertNickles()
+        Nickles = Nickles + 1
+    End Sub
+    Public Sub insertDimes()
+        Dimes = Dimes + 1
+    End Sub
+    Public Sub insertPennies()
+        Pennies = Pennies + 1
+    End Sub
+    Public Sub insertDollars()
+        Dollars = Dollars + 1
+    End Sub
+End Class
