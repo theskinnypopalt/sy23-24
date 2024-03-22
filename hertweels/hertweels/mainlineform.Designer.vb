ddd8e08aebf0b0c0413cdc6cb20c5e99c9c5cc06
Tanner <tkset494@learner.dvusd.org>
2024-03-22 08:11:19 -0700

    🤓

--- hertweels/hertweels/mainlineform.Designer.vb
+++ hertweels/hertweels/mainlineform.Designer.vb
@@ -23,6 +23,16 @@ Partial Class mainlineform
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.homebutton = New System.Windows.Forms.Button()
+        Me.Sth5 = New hertweels.STH()
+        Me.Sth6 = New hertweels.STH()
+        Me.Sth7 = New hertweels.STH()
+        Me.Sth8 = New hertweels.STH()
+        Me.Sth9 = New hertweels.STH()
+        Me.Sth10 = New hertweels.STH()
+        Me.Sth4 = New hertweels.STH()
+        Me.Sth3 = New hertweels.STH()
+        Me.Sth2 = New hertweels.STH()
+        Me.Sth1 = New hertweels.STH()
         Me.SuspendLayout()
         '
         'homebutton
@@ -34,12 +44,132 @@ Partial Class mainlineform
         Me.homebutton.Text = "HOME"
         Me.homebutton.UseVisualStyleBackColor = True
         '
+        'Sth5
+        '
+        Me.Sth5.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Sth5.carname = Nothing
+        Me.Sth5.Location = New System.Drawing.Point(12, 156)
+        Me.Sth5.Name = "Sth5"
+        Me.Sth5.picture = Nothing
+        Me.Sth5.price = 0
+        Me.Sth5.Size = New System.Drawing.Size(204, 109)
+        Me.Sth5.TabIndex = 5
+        '
+        'Sth6
+        '
+        Me.Sth6.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Sth6.carname = Nothing
+        Me.Sth6.Location = New System.Drawing.Point(432, 156)
+        Me.Sth6.Name = "Sth6"
+        Me.Sth6.picture = Nothing
+        Me.Sth6.price = 0
+        Me.Sth6.Size = New System.Drawing.Size(204, 109)
+        Me.Sth6.TabIndex = 6
+        '
+        'Sth7
+        '
+        Me.Sth7.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Sth7.carname = Nothing
+        Me.Sth7.Location = New System.Drawing.Point(222, 156)
+        Me.Sth7.Name = "Sth7"
+        Me.Sth7.picture = Nothing
+        Me.Sth7.price = 0
+        Me.Sth7.Size = New System.Drawing.Size(204, 109)
+        Me.Sth7.TabIndex = 7
+        '
+        'Sth8
+        '
+        Me.Sth8.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Sth8.carname = Nothing
+        Me.Sth8.Location = New System.Drawing.Point(642, 156)
+        Me.Sth8.Name = "Sth8"
+        Me.Sth8.picture = Nothing
+        Me.Sth8.price = 0
+        Me.Sth8.Size = New System.Drawing.Size(204, 109)
+        Me.Sth8.TabIndex = 8
+        '
+        'Sth9
+        '
+        Me.Sth9.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Sth9.carname = "Porsche 911 GT3"
+        Me.Sth9.Location = New System.Drawing.Point(852, 41)
+        Me.Sth9.Name = "Sth9"
+        Me.Sth9.picture = Global.hertweels.My.Resources.Resources.hotwheels5
+        Me.Sth9.price = 7
+        Me.Sth9.Size = New System.Drawing.Size(204, 109)
+        Me.Sth9.TabIndex = 9
+        '
+        'Sth10
+        '
+        Me.Sth10.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Sth10.carname = Nothing
+        Me.Sth10.Location = New System.Drawing.Point(852, 156)
+        Me.Sth10.Name = "Sth10"
+        Me.Sth10.picture = Nothing
+        Me.Sth10.price = 0
+        Me.Sth10.Size = New System.Drawing.Size(204, 109)
+        Me.Sth10.TabIndex = 10
+        '
+        'Sth4
+        '
+        Me.Sth4.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Sth4.carname = "Saleen S7 Blue"
+        Me.Sth4.Location = New System.Drawing.Point(642, 41)
+        Me.Sth4.Name = "Sth4"
+        Me.Sth4.picture = Global.hertweels.My.Resources.Resources.hotwheels4
+        Me.Sth4.price = 12
+        Me.Sth4.Size = New System.Drawing.Size(204, 109)
+        Me.Sth4.TabIndex = 4
+        '
+        'Sth3
+        '
+        Me.Sth3.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Sth3.carname = "Czinger 21C 13/250"
+        Me.Sth3.Location = New System.Drawing.Point(432, 41)
+        Me.Sth3.Name = "Sth3"
+        Me.Sth3.picture = Global.hertweels.My.Resources.Resources.hotwheels3
+        Me.Sth3.price = 3
+        Me.Sth3.Size = New System.Drawing.Size(204, 109)
+        Me.Sth3.TabIndex = 3
+        '
+        'Sth2
+        '
+        Me.Sth2.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Sth2.carname = "Peak Tacoma"
+        Me.Sth2.Location = New System.Drawing.Point(222, 41)
+        Me.Sth2.Name = "Sth2"
+        Me.Sth2.picture = Global.hertweels.My.Resources.Resources.hotwheels2
+        Me.Sth2.price = 9
+        Me.Sth2.Size = New System.Drawing.Size(204, 109)
+        Me.Sth2.TabIndex = 2
+        '
+        'Sth1
+        '
+        Me.Sth1.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Sth1.carname = "2017 Nissan GT-R R35"
+        Me.Sth1.Location = New System.Drawing.Point(12, 41)
+        Me.Sth1.Name = "Sth1"
+        Me.Sth1.picture = Global.hertweels.My.Resources.Resources.hotwheels11
+        Me.Sth1.price = 2
+        Me.Sth1.Size = New System.Drawing.Size(204, 109)
+        Me.Sth1.TabIndex = 1
+        '
         'mainlineform
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.DeepSkyBlue
         Me.ClientSize = New System.Drawing.Size(1220, 646)
+        Me.Controls.Add(Me.Sth10)
+        Me.Controls.Add(Me.Sth9)
+        Me.Controls.Add(Me.Sth8)
+        Me.Controls.Add(Me.Sth7)
+        Me.Controls.Add(Me.Sth6)
+        Me.Controls.Add(Me.Sth5)
+        Me.Controls.Add(Me.Sth4)
+        Me.Controls.Add(Me.Sth3)
+        Me.Controls.Add(Me.Sth2)
+        Me.Controls.Add(Me.Sth1)
         Me.Controls.Add(Me.homebutton)
         Me.Name = "mainlineform"
         Me.Text = "mainlineform"
@@ -48,4 +178,14 @@ Partial Class mainlineform
     End Sub
 
     Friend WithEvents homebutton As Button
+    Friend WithEvents Sth1 As STH
+    Friend WithEvents Sth2 As STH
+    Friend WithEvents Sth3 As STH
+    Friend WithEvents Sth4 As STH
+    Friend WithEvents Sth5 As STH
+    Friend WithEvents Sth6 As STH
+    Friend WithEvents Sth7 As STH
+    Friend WithEvents Sth8 As STH
+    Friend WithEvents Sth9 As STH
+    Friend WithEvents Sth10 As STH
 End Class
