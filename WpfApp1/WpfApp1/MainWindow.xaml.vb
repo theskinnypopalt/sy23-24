4681b0225dada09c690532f6a9d969772e62ca18
theskinnypopalt <bmste396@learner.dvusd.org>
2024-04-18 08:20:13 -0700

    carsevtdbnfh,ijk.;/'l

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub button1_Click(sender As Object, e As RoutedEventArgs) Handles button1.Click
+        label1.Content = "WHATS UP BROTHER!"
+        label2.Content = "SPECIAL TEAMS"
+        label3.Content = "SPECIAL PLAYS"
+        label4.Content = "SPECIAL PLAYERS"
+        label5.Content = "TUESDAY! TUESDAY!"
+    End Sub
+End Class
---
+++ credit card payment/credit card payment/MainWindow.xaml.vb
@@ -0,0 +1,7 @@
+﻿Class MainWindow
+    Private Sub confirm_purchase_button_Click(sender As Object, e As RoutedEventArgs) Handles confirm_purchase_button.Click
+        If first_name_textbox.Text = "" Then
+            first_name_textbox.Text = "THIS MUST BE FILLED IN!"
+        End If
+    End Sub
+End Class
