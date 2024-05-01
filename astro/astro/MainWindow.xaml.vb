a74f41657d377152baec7e0d2d2eb67f431b0bbc
theskinnypopalt <bmste396@learner.dvusd.org>
2024-05-01 08:13:35 -0700

    paint program

--- astro/astro/MainWindow.xaml.vb
+++ astro/astro/MainWindow.xaml.vb
@@ -6,8 +6,53 @@
         label1.Content = calender1.SelectedDate
         m = calender1.SelectedDate.Value.Month
         d = calender1.SelectedDate.Value.Day
-        If m = 4 And d > 20 Or m = 5 And d < 20 Then
+        If m = 3 And d > 20 Or m = 4 And d < 20 Then
             astrosignlabel.Content = "Aries"
+            piclabel.Content = "♈️"
+        End If
+        If m = 4 And d > 20 Or m = 5 And d < 20 Then
+            astrosignlabel.Content = "Taurus"
+            piclabel.Content = "♉️"
+        End If
+        If m = 5 And d > 20 Or m = 6 And d < 20 Then
+            astrosignlabel.Content = "Gemini"
+            piclabel.Content = "♊️"
+        End If
+        If m = 6 And d > 20 Or m = 7 And d < 22 Then
+            astrosignlabel.Content = "Cancer"
+            piclabel.Content = "♋️"
+        End If
+        If m = 7 And d > 23 Or m = 8 And d < 22 Then
+            astrosignlabel.Content = "leo"
+            piclabel.Content = "♌️"
+        End If
+        If m = 8 And d > 22 Or m = 9 And d < 22 Then
+            astrosignlabel.Content = "Virgo"
+            piclabel.Content = "♍️"
+        End If
+        If m = 9 And d > 22 Or m = 10 And d < 21 Then
+            astrosignlabel.Content = "Libra"
+            piclabel.Content = "♎️"
+        End If
+        If m = 10 And d > 21 Or m = 11 And d < 21 Then
+            astrosignlabel.Content = "Scorpio"
+            piclabel.Content = "♏️"
+        End If
+        If m = 11 And d > 21 Or m = 12 And d < 21 Then
+            astrosignlabel.Content = "Sagittarius"
+            piclabel.Content = "♐️"
+        End If
+        If m = 12 And d > 21 Or m = 1 And d < 19 Then
+            astrosignlabel.Content = "Capricorn"
+            piclabel.Content = "♑️"
+        End If
+        If m = 1 And d > 19 Or m = 2 And d < 18 Then
+            astrosignlabel.Content = "Aquarius"
+            piclabel.Content = "♒️"
+        End If
+        If m = 2 And d > 18 Or m = 3 And d < 20 Then
+            astrosignlabel.Content = "Pisces"
+            piclabel.Content = "♓️"
         End If
     End Sub
 End Class
---
+++ mypaint/mypaint/MainWindow.xaml.vb
@@ -0,0 +1,36 @@
+﻿Class MainWindow
+    Private Sub drawingCanvas_MouseMove(sender As Object, e As MouseEventArgs) Handles drawingCanvas.MouseMove
+        Dim el As New Ellipse
+        el.Width = widthSlider.Value
+        el.Height = heightSlider.Value
+        el.Fill = color1Rectangle.Fill
+
+        Dim p As Point = Mouse.GetPosition(drawingCanvas)
+        Canvas.SetLeft(el, p.X)
+        Canvas.SetTop(el, p.Y)
+        If e.MouseDevice.LeftButton = MouseButtonState.Pressed Then
+            mainCanvas.Children.Add(el)
+        End If
+    End Sub
+
+    Private Sub color1Rectangle_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles color1Rectangle.MouseDown, color2rectangle.MouseDown, color3rectangle.MouseDown, color4rectangle.MouseDown, color5rectangle.MouseDown, color6rectangle.MouseDown
+        color1Rectangle.Fill = sender.fill
+    End Sub
+
+    Private Sub clearbutton_Click(sender As Object, e As RoutedEventArgs) Handles clearbutton.Click
+        mainCanvas.Children.RemoveRange(1, mainCanvas.Children.Count - 1)
+    End Sub
+
+    Private Sub undobutton_Click(sender As Object, e As RoutedEventArgs) Handles undobutton.Click
+        mainCanvas.Children.RemoveAt(mainCanvas.Children.Count - 1)
+    End Sub
+
+    Private Sub elipsebutton_Click(sender As Object, e As RoutedEventArgs) Handles elipsebutton.Click
+        shapelabel.Content = sender.content
+    End Sub
+
+    Private Sub rectanglebutton_Click(sender As Object, e As RoutedEventArgs) Handles rectanglebutton.Click
+        shapelabel.Content = sender.content
+    End Sub
+End Class
+
